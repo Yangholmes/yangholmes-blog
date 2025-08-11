ae97cc8b33d9d0896ecb1d9f6274284ba29f7963
yangholmes <yangholmes@126.com>
2025-08-11 12:19:16 +0800

    feat: [theme]替换评论插件

--- docs/.vitepress/theme/composables/useComment.ts
+++ docs/.vitepress/theme/composables/useComment.ts
@@ -9,14 +9,33 @@ export function useComment(el: ShallowRef<HTMLDivElement | null>) {
   watch(() => el.value, () => {
     if (!el.value) return;
 
+    // let script = document.createElement('script');
+    // script.src = 'https://utteranc.es/client.js';
+    // script.setAttribute('repo', 'Yangholmes/yangholmes-blog');
+    // script.setAttribute('issue-term', 'pathname');
+    // script.setAttribute('theme', isDark.value ? 'github-dark' : 'github-light');
+    // script.setAttribute('crossorigin', 'anonymous');
+    // el.value.appendChild(script);
+    // scriptRef.value = script
+
     let script = document.createElement('script');
-    script.src = 'https://utteranc.es/client.js';
-    script.setAttribute('repo', 'Yangholmes/yangholmes-blog');
-    script.setAttribute('issue-term', 'pathname');
-    script.setAttribute('theme', isDark.value ? 'github-dark' : 'github-light');
+    script.src = 'https://giscus.app/client.js';
+    script.setAttribute('data-repo', 'Yangholmes/yangholmes-blog'); // 仓库
+    script.setAttribute('data-repo-id', 'R_kgDOPZRnnA'); // 仓库 id
+    script.setAttribute('data-category', 'Announcements');
+    script.setAttribute('data-category-id', 'DIC_kwDOPZRnnM4CuBm9'); // 分类 id
+    script.setAttribute('data-mapping', 'pathname');
+    script.setAttribute('data-strict', '1');
+    script.setAttribute('data-reactions-enabled', '1');
+    script.setAttribute('data-emit-metadata', '0');
+    script.setAttribute('data-input-position', 'top');
+    script.setAttribute('data-theme', isDark.value ? 'dark' : 'light');
+    script.setAttribute('data-lang', 'zh-CN');
+    script.setAttribute('data-loading', 'lazy');
     script.setAttribute('crossorigin', 'anonymous');
+    script.setAttribute('async', '1');
     el.value.appendChild(script);
-    scriptRef.value = script;
+    scriptRef.value = script
   })
 
   onUnmounted(() => {
