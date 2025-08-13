e718fa3bdbca3b11916a2a88d214203c82014e53
yangholmes <yangholmes@126.com>
2025-08-13 17:07:08 +0800

    chore(theme): 修复一个 hash 跳转的问题

--- docs/.vitepress/theme/Page.vue
+++ docs/.vitepress/theme/Page.vue
@@ -1,15 +1,55 @@
 <script lang="ts" setup>
-import {useTemplateRef} from 'vue';
+import {useTemplateRef, onMounted, nextTick, ref, onUnmounted} from 'vue';
 import {useComment} from './composables/useComment';
 
+  const pageRef = useTemplateRef('page-ref');
   const commentRef = useTemplateRef('comment-ref');
 
-  useComment(commentRef)
+  useComment(commentRef);
+
+  // function goHash() {
+  //   const { hash } = location;
+  //   const target = document.getElementById(decodeURIComponent(hash).slice(1));
+  //   target ? target.scrollIntoView({
+  //     block: 'start',
+  //     behavior: 'smooth'
+  //   }): pageRef.value?.scrollTo(0, 0);
+  // }
+
+  const controller = ref<AbortController>();
+  onMounted(() => {
+    nextTick(() => {
+      controller.value = new AbortController();
+      pageRef.value?.addEventListener('click', (evt) => {
+        const link = (evt.target as (HTMLElement | null))?.closest?.('a');
+        if (!link) {
+          return;
+        }
+        const linkHref = link.getAttribute('href') || '';
+        const { origin, hash } = new URL(linkHref, link.baseURI);
+        if (origin === location.origin && hash) {
+          const target = link.classList.contains('header-anchor')
+            ? link
+            : document.getElementById(decodeURIComponent(hash).slice(1));
+          target?.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth'
+          });
+        }
+      }, {
+        signal: controller.value.signal
+      });
+    });
+  });
+
+  onUnmounted(() => {
+    controller.value?.abort();
+  });
 
 </script>
 
 <template>
-  <div class="page">
+  <div class="page" ref="page-ref">
     <div class="vp-doc">
       <Content />
     </div>
@@ -30,6 +70,25 @@ import {useComment} from './composables/useComment';
     width: 100%;
     /* margin-top: 3rem; */
     /* height: 100%; */
+
+    :deep(.table-of-contents) {
+      width: auto;
+      padding: 1.25rem 1.25rem 1.25rem 0;
+
+      display: inline-block;
+
+      border: 2px solid var(--color--level-3);
+      border-radius: 20px;
+
+      ul {
+        margin: 0;
+        list-style: none;
+      }
+
+      a {
+        text-decoration: none;
+      }
+    }
   }
 }
 </style>
